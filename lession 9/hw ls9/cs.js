2ef8766a22703c89070cbb8e89687da5d2662c1e
ha <habobn@gmail.com>
2018-12-27 00:47:16 +0700

    ha

--- lession 9/hw ls9/cs.js
+++ lession 9/hw ls9/cs.js
@@ -1,10 +1,39 @@
-var box = document.getElementsByClassName("box");
-for (var i = 0; i < box.length; i++) {
-	box[i].addEventListener("click",function(){
-
-	var active = document.getElementsByClassName("active")[0];
-	active.className ="picture";
-
-	this.children.className = "picture active";
-	});
+var picture=document.getElementsByClassName('picture');
+var box= document.getElementsByClassName('box');
+var dem=0;
+var length= picture.length;
+function left(){
+	dem--;
+	for (var i = picture.length -1; i >= 0; i--) {
+		picture[i].className='picture';
+		box[i].className='box';
+	}
+	if(dem<0){
+				dem=picture.length-1;
+			}
+		picture[dem].className='picture active';
+		box[dem].className='box action';
 }
+function right(){
+	dem++;
+	for (var i = picture.length -1; i >= 0; i--) {
+		picture[i].className='picture';
+		box[i].className='box';
+	}
+	if(dem>picture.length-1){
+				dem=0;
+			}
+		picture[dem].className='picture active';
+		box[dem].className='box action';
+}
+
+function icon(n){
+		for (var i = picture.length-1; i >=0; i--) {
+			picture[i].className='picture';
+			box[i].className='box';
+	}
+		dem=n;
+		picture[dem].className='picture active';
+		box[dem].className='box action';
+}
+window.setInterval(function(){ right(); }, 10000);
